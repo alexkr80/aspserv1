8941363500589b490832b310dfa1b95497299db2
Володин Александр Владимирович <krivosheev@mkb.mari.ru>
2020-02-14 10:27:57 +0300

    Добавьте файлы проекта.

---
+++ Services/TimerMiddleware.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspserv1.Services
+{
+    public class TimerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        /*TimeService _timeService;
+
+        public TimerMiddleware(RequestDelegate next, TimeService timeService)
+        {
+            _next = next;
+            _timeService = timeService;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (context.Request.Path.Value.ToLower() == "/time")
+            {
+                context.Response.ContentType = "text/html; charset=utf-8";
+                await context.Response.WriteAsync($"Текущее время: {_timeService?.Time}");
+            }
+            else
+            {
+                await _next.Invoke(context);
+            }
+        }*/
+
+        public TimerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, TimeService timeService)
+        {
+            if (context.Request.Path.Value.ToLower() == "/time")
+            {
+                context.Response.ContentType = "text/html; charset=utf-8";
+                await context.Response.WriteAsync($"Текущее время: {timeService?.Time}");
+            }
+            else
+            {
+                await _next.Invoke(context);
+            }
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using aspserv1.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace aspserv1
+{
+    public class Startup
+    {
+        // private IServiceCollection _services;
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IMessageSender, EmailMessageSender>();
+            services.AddTransient<TimeService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IMessageSender messageSender)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IMessageSender messageSender)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMiddleware<TimerMiddleware>();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync(messageSender.Send());
+            });
+
+
+        }
+    }
+}
